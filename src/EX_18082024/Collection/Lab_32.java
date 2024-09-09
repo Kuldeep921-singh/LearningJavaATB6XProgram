e6435a91f4cbf3d8b82ef73f30aebd0cad40114a
kushs <kush.singh.921@gmail.com>
2024-09-09 13:17:59 +0530

    Collection Framawork 3

---
+++ src/EX_18082024/Collection/Lab_32.java
@@ -0,0 +1,18 @@
+package EX_18082024.Collection;
+
+import java.util.ArrayList;
+
+public class Lab_32 {
+
+    public static void main(String[] args) {
+
+        ArrayList ne = new ArrayList();
+
+        ne.add(43);
+        ne.add("intelligent");
+        ne.add(234.23);
+
+        System.out.println(ne);
+
+    }
+}
